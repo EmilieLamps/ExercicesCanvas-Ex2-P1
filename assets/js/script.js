198b814ad350e7748c0790ad5cedd06dfb56aee3
EmilieLamps <emilie.lamps@gmail.com>
2019-11-22 16:07:07 +0100

    Modification du JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,23 @@ var c = document.getElementById("mon_canvas");
 var ctx = c.getContext("2d");
 
 //Toit de la maison
- ctx.beginPath();
- ctx.moveTo(200,60); // Point de départ (Gauche, Haut)
- ctx.lineTo(285,155); // ligne de droite du triangle
- ctx.lineTo(110,155); // ligne de gauche du triangle
- ctx.closePath();
- ctx.fillStyle = "#D28143"; // Définition de la couleur de remplissage
- ctx.fill();
+ctx.rotate(Math.PI/4);
+ctx.fillStyle = "#D28143";
+ctx.fillRect(191, -95, 123, 123);
 
 // Forme de la maison
+ctx.rotate(Math.PI/4)
 ctx.fillStyle = "lightblue";
-ctx.fillRect(110, 155, 175, 175);
+ctx.fillRect(155, -290, 175, 175);
 
 //  Fenêtre de gauche
 ctx.fillStyle = "#C9C9C9"
-ctx.fillRect(135, 180, 30, 30);
+ctx.fillRect(180, -165, 30, 30);
 
 // Fenêtre de droite
 ctx.fillStyle = "#C9C9C9"
-ctx.fillRect(230, 180, 30, 30);
+ctx.fillRect(180, -270, 30, 30);
 
 // Porte
 ctx.fillStyle = "#C9C9C9"
-ctx.fillRect(177, 280, 40, 50);
+ctx.fillRect(280, -220, 50, 40);
